91f84623cd6516f849dba1c380293c6df83befdf
RenatoUbuntu <renatoyferreira@gmail.com>
2023-05-30 16:05:09 -0300

    feat:signin back

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -18,3 +18,28 @@ export async function signUp(req, res) {
     res.status(500).send(err.message);
   }
 }
+
+
+export async function signIn(req, res) {
+    const { email, password } = req.body;
+  
+    try {
+        const user = await db.query(`SELECT * FROM users WHERE email=$1`, [email])
+      if(!user) return res.status(404).send("Not found user")
+  
+      const isPasswordCorrect = bcrypt.compareSync(password, user.password)
+      if(!isPasswordCorrect) return res.status(401).send("Não autorizado")
+  
+      const token = uuid()
+      
+      await db.query(`
+      INSERT INTO sessions (user, token)
+      VALUES ($1, $2)`, 
+      [email, token])
+
+      res.send(token)
+  
+    } catch (err) {
+      res.status(500).send(err.message);
+    }
+  }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,11 @@
 import {Router} from "express"
-import { signUp } from "../controllers/auth.controllers.js"
+import { signUp, signIn } from "../controllers/auth.controllers.js"
 import validateSchema from "../middleware/validateSchema.middleware.js"
-import { userSchema } from "../schemas/auth.schema.js"
+import { userSchema, loginSchema } from "../schemas/auth.schema.js"
 
 const authRouter = Router()
 
 authRouter.post("/sign-up", validateSchema(userSchema), signUp)
-authRouter.post("/sign-in")
+authRouter.post("/sign-in", validateSchema(loginSchema), signIn )
 
 export default authRouter
