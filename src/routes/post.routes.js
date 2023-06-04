1a2b26f03783b9b122d4236378c7b6bbaa7caa86
RenatoUbuntu <renatoyferreira@gmail.com>
2023-06-04 16:57:40 -0300

    fixed: bug getpost

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -7,7 +7,7 @@ import { postSchema, updateSchema } from "../schemas/post.schema.js";
 const postRouter = Router();
 
 postRouter.post("/post", validateSchema(postSchema), validateAuth, sendPost);
-postRouter.get("/posts", getPost); // Rota para obter todos os posts
+postRouter.get("/post", getPost); // Rota para obter todos os posts
 postRouter.get("/hashtag/:hashtag", getPost); // Rota para obter posts por hashtag
 postRouter.delete("/post", validateAuth, deletePost);
 postRouter.put("/post", validateSchema(updateSchema), validateAuth, updatePost);
