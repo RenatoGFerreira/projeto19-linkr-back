51a2a13d7115f3c1f8eea8cb20a737f41a65ad35
RenatoUbuntu <renatoyferreira@gmail.com>
2023-06-04 06:08:46 -0300

    refactor

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,15 +1,17 @@
 import { Router } from "express";
-import { getPost, sendPost } from "../controllers/post.controllers.js";
+import { getPost, sendPost, deletePost, updatePost } from "../controllers/post.controllers.js";
 import validateAuth from "../middleware/validateAuth.middleware.js";
 import validateSchema from "../middleware/validateSchema.middleware.js";
-import { postSchema } from "../schemas/post.schema.js";
-import extractHashtags from "../middleware/extractHashtags.middleware.js";
+import { postSchema, updateSchema } from "../schemas/post.schema.js";
+// import extractHashtags from "../middleware/extractHashtag.middleware.js";
 import { getPostsByHashtag, getHashtagRanking } from "../controllers/hashtag.controllers.js";
 
 const postRouter = Router();
 
-postRouter.post("/post", validateSchema(postSchema), validateAuth, extractHashtags, sendPost);
+postRouter.post("/post", validateSchema(postSchema), validateAuth, sendPost);
 postRouter.get("/post", getPost);
+postRouter.delete("/post", validateAuth, deletePost);
+postRouter.put("/post", validateSchema(updateSchema), validateAuth, updatePost);
 postRouter.get("/hashtags/:hashtag", getPostsByHashtag);
 postRouter.get("/hashtags", getHashtagRanking);
 
